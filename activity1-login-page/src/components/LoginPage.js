461036998da6ac3b6b084d4d2aeeba79e228d3a5
FTsune <s2021102102@firstasia.edu.ph>
2024-02-06 11:36:58 +0800

    new commit

---
+++ activity1-login-page/src/components/LoginPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div className="login-page">
+            <h2>Login</h2>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPage;
