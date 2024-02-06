031bef9422da6b3796113b5a6b19f7f185ca6ea1
FTsune <s2021102102@firstasia.edu.ph>
2024-02-06 21:47:28 +0800

    new style added

--- activity1-login-page/src/components/LoginPage.js
+++ activity1-login-page/src/components/LoginPage.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginPage.css';
 
 const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleShowPasswordToggle = () => {
+        setShowPassword(!showPassword);
+    }
+
     return (
         <div className="login-page">
-            <h2>Login</h2>
+            <h2 className="login-heading">Login</h2>
             <form>
                 <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" name="username" />
+                    <input 
+                        type="text" 
+                        id="username" 
+                        name="username" 
+                        value={username} 
+                        onChange={handleUsernameChange} 
+                        placeholder="Username" 
+                    />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" />
+                    <input 
+                        type={showPassword ? "text" : "password"} 
+                        id="password" 
+                        name="password" 
+                        value={password} 
+                        onChange={handlePasswordChange} 
+                        placeholder="Password" 
+                    />
+                    <label htmlFor="showPassword">
+                        <input 
+                            type="checkbox" 
+                            id="showPassword" 
+                            name="showPassword" 
+                            checked={showPassword} 
+                            onChange={handleShowPasswordToggle} 
+                        />
+                        <span>Show Password</span>
+                    </label>
                 </div>
                 <button type="submit">Login</button>
             </form>
